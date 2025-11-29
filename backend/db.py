7d13876c0dbdb54026025c0e96f9450ae059bc2e
kennardliong <kennardliong1@gmail.com>
2025-11-28 20:18:38 -0500

    Use environment variable for MongoDB URI
    Updated MongoDB connection to use environment variable for URI.

--- backend/db.py
+++ backend/db.py
@@ -2,10 +2,9 @@ from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # load your .env file
-
-# MONGODB_URI = "mongodb+srv://tokotalk-bot:liong0506@tokotalk-cluster.toh76ct.mongodb.net/?retryWrites=true&w=majority&appName=tokotalk-cluster"
-uri = "mongodb+srv://tokotalk-bot:liong0506@tokotalk-cluster.toh76ct.mongodb.net/?appName=tokotalk-cluster"
+uri = os.getenv("MONGODB_URI")
+if not uri:
+    raise RuntimeError("MONGODB_URI is not set in the environment.")
 
 client = MongoClient(uri)
 db = client["tokotalk-bot"]
