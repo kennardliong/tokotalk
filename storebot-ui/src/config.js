b86dd53429f5561083feefd2caf82100932f2559
Kennard Liong <kennardliong1@gmail.com>
2025-11-28 21:41:25 -0500

    Update config.js with deployment instructions
    Added comments for environment variable usage in config.

--- storebot-ui/src/config.js
+++ storebot-ui/src/config.js
@@ -1,3 +1,7 @@
 // src/config.js
 export const API_BASE_URL =
   process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+
+//when deploying on render set an environment variable where REACT_APP_API_URL = link to the backend
+// REACT_APP_API_URL = https://tokotalk-backend.onrender.com
